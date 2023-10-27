76d493d390cefadc4f76b569dbcc58486ecff996
MariSychikova <148006311+MariSychikova@users.noreply.github.com>
2023-10-27 09:06:33 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_2.py
@@ -0,0 +1,12 @@
+def find_common_participants(participants_1, participants_2, separator=','):
+    set_participants_1 = set(participants_1.split(separator))
+    list_common_participants = sorted(list(set_participants_1.intersection(participants_2.split(separator))))
+
+    return list_common_participants
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+common_participants = find_common_participants(participants_first_group, participants_second_group, separator='|')
+print("Общие участники:", common_participants)
